3e01938e1411ec6fd47569d686e143b3c81adb18
nikolajmunk <munk.nikolaj@gmail.com>
2018-10-25 15:36:50 +0200

    ignore fix

--- 2018 KADK Fall Game Jam/Assets/spawnAds.cs
+++ 2018 KADK Fall Game Jam/Assets/spawnAds.cs
@@ -8,7 +8,12 @@ public class spawnAds : MonoBehaviour {
     public float timeBtwSpawn = 1f;
     public float elapsedTime = 0f;
     public Vector2 spawnLocationRange = new Vector2(0f, 0f);
-    public GameObject spawner = GameObject.FindGameObjectWithTag("Spawner");
+    public GameObject spawner;
+
+    void Start()
+    {
+        spawner = GameObject.FindGameObjectWithTag("Spawner");
+    }
 
     // Update is called once per frame
     void Update()
@@ -23,7 +28,6 @@ public class spawnAds : MonoBehaviour {
     }
 
     void spawnAd (GameObject ad) {
-
         Vector3 spawnLocation = new Vector3(Random.Range(-spawnLocationRange.x, spawnLocationRange.x), Random.Range(-spawnLocationRange.y, spawnLocationRange.y), 0f);
         Instantiate(ad, spawnLocation, Quaternion.identity, spawner.transform);
         }
