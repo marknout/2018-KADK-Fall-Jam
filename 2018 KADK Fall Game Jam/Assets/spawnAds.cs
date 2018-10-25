df42447a6e3cafa58b941d1cfe4a6afa00c0f6df
nikolajmunk <munk.nikolaj@gmail.com>
2018-10-25 15:01:59 +0200

    Updated gitignore and added scene for testing ad spawn.

---
+++ 2018 KADK Fall Game Jam/Assets/spawnAds.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spawnAds : MonoBehaviour {
+
+    public GameObject[] ads;
+    public float timeBtwSpawn = 1f;
+    public float elapsedTime = 0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        elapsedTime += Time.deltaTime;
+
+        if (elapsedTime > timeBtwSpawn)
+        {
+            elapsedTime = 0;
+            spawnAd(ads[Random.Range(0, ads.Length)]);
+        }
+    }
+
+    void spawnAd (GameObject ad) {
+        Instantiate(ad, Vector3.zero, Quaternion.identity);
+        }
+    }
