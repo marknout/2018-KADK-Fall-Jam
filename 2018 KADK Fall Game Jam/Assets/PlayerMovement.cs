53d8dcb7cf681bde60b5510791f44a2e65142836
Bjørn <bjso1815@edu.kadk.dk>
2018-10-25 14:28:15 +0200

    MOOOOve in new scene

---
+++ 2018 KADK Fall Game Jam/Assets/PlayerMovement.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+    public Rigidbody RB;
+
+    public float ForwardForce = 200f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate() {
+
+        RB.AddForce(ForwardForce * Time.deltaTime, 0,0);
+
+	}
+}
