7bbd72cdff4471fd8d86b0605b93ae770964a12e
Bjørn <bjso1815@edu.kadk.dk>
2018-10-26 13:12:12 +0200

    Countdown & loadLevel

---
+++ 2018 KADK Fall Game Jam/Assets/Scripts/CountdownUI.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class CountdownUI : MonoBehaviour
+{
+
+    [SerializeField] private Text UIText;
+    [SerializeField] private float MainTimer;
+
+    private float Timer;
+
+    void Start()
+    {
+
+        Timer = MainTimer;
+
+    }
+
+    void Update()
+    {
+    
+        if (Timer >= 0.0f)
+        {
+            Timer -= Time.deltaTime;
+            UIText.text = Timer.ToString("F");
+        }
+
+        else if (Timer <= 0.0f)
+        {
+            UIText.text = "0.00";
+            Timer = 0.0f;
+            SceneManager.LoadScene("TestLevel");
+        }
+
+    }
+}
